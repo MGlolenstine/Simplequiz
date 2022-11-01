d80e13911a1502638dd6b39ccf2bd4f1b0b976a1
MGlolenstine <mglolenstine@gmail.com>
2022-11-01 09:17:58 +0100

    Implemented start_game button.

--- app/src/main/java/com/example/quizappforassignment2/MainActivity.java
+++ app/src/main/java/com/example/quizappforassignment2/MainActivity.java
@@ -10,107 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    TextView totalQuestionsTextView;
-    TextView questionTextView;
-    Button ansA, ansB, ansC, ansD;
-    Button submitBtn;
-
-    int score=0;
-    int totalQues=QnA.question.length;
-    int currentQuestionIndex = 0;
-    String selectedAnswer = "";
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        totalQuestionsTextView = findViewById(R.id.total_question);
-        questionTextView = findViewById(R.id.ques);
-        ansA = findViewById(R.id.ans_A);
-        ansB = findViewById(R.id.ans_B);
-        ansC = findViewById(R.id.ans_C);
-        ansD = findViewById(R.id.ans_D);
-        submitBtn = findViewById(R.id.submit);
-
-        ansA.setOnClickListener(this);
-        ansB.setOnClickListener(this);
-        ansC.setOnClickListener(this);
-        ansD.setOnClickListener(this);
-        submitBtn.setOnClickListener(this);
-
-        totalQuestionsTextView.setText("Total Questions :"+ totalQues);
-
-        loadNewQuestion();
-
-
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        ansA.setBackgroundColor(Color.WHITE);
-        ansB.setBackgroundColor(Color.WHITE);
-        ansC.setBackgroundColor(Color.WHITE);
-        ansD.setBackgroundColor(Color.WHITE);
-
-
-        Button clickedButton = (Button) view;
-        if(clickedButton.getId()==R.id.submit){
-            if(selectedAnswer.equals(QnA.correctAnswers[currentQuestionIndex])){
-                score++;
-            }
-            currentQuestionIndex++;
-            loadNewQuestion();
-
-
-        }else{
-
-            selectedAnswer = clickedButton.getText().toString();
-            clickedButton.setBackgroundColor(Color.YELLOW);
-
-        }
-
-    }
-
-    void loadNewQuestion(){
-
-        if(currentQuestionIndex == totalQues){
-            finishQuiz();
-            return;
-        }
-
-        questionTextView.setText(QnA.question[currentQuestionIndex]);
-        ansA.setText(QnA.choices[currentQuestionIndex][0]);
-        ansB.setText(QnA.choices[currentQuestionIndex][1]);
-        ansC.setText(QnA.choices[currentQuestionIndex][2]);
-        ansD.setText(QnA.choices[currentQuestionIndex][3]);
-
-    }
-
-    void finishQuiz(){
-        String passStatus = "";
-        if(score > totalQues*0.60) {
-            passStatus = "Passed";
-        }else{
-            passStatus = "Failed";
-        }
-
-        new AlertDialog.Builder(this)
-                .setTitle(passStatus)
-                .setMessage( "Score is " + score + "out of " + totalQues )
-                .setPositiveButton("Restart the Quiz",(dialogInterface, i) -> restartQuiz())
-                .setCancelable(false)
-                .show();
-
-    }
-
-    void restartQuiz(){
-        score = 0;
-        currentQuestionIndex =0;
-        loadNewQuestion();
+        findViewById(R.id.start_game).setOnClickListener(view -> {
+            Intent intent = new Intent(this, QnAActivity.class);
+            startActivity(intent);
+        });
     }
 }
